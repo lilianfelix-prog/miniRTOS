e8c8b44c3022844654853b25d16f5418c36f980d
lilianfelix-prog <felenclilian2018@gmail.com>
2025-06-11 23:26:24 -0400

    very basic sbrk func in riscv assembly using the linker addrs

--- heap_1.c
+++ heap_1.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
+extern void* _sbrk(__intptr_t increment);
 
-void * heap1Malloc(size_t size)
+void* heap1Malloc(size_t size)
 {
+    if(size == 0){
+        return NULL;
+    }
+    void* block = _sbrk(size);
+    return block;
     
 }
